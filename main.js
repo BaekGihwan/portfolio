d3433502868146fd44a2ec4ced4588c71ab7de27
gihwan <ghbaek88@gmail.com>
2020-11-09 22:11:29 +0900

    First finish

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar-dark');
+  } else {
+    navbar.classList.remove('navbar-dark');
+  }
+});
+
+// handle
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {  
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  scrollIntoView(link);
+});
+
+// handle button
+const homeContactBtn = document.querySelector('.home_contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector){
+  const scrollTO = document.querySelector(selector);
+  scrollTO.scrollIntoView({behavior: "smooth"});
+}
